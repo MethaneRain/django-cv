499c479414dce9b31b108cd7b1d712439e648f98
MethaneRain <36716747+MethaneRain@users.noreply.github.com>
2021-06-08 19:04:23 +0000

    Update models.py

--- src/projects/models.py
+++ src/projects/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    title = models.TextField()#models.CharField()
+    description = models.TextField()
+    price = models.TextField()#models.DecimalField()
+    summary = models.TextField(default="Write a summary")
